e1a8bf66cc190bbb9b02f80319f340b15a1eeb02
Merk42 <mark_c@markecurtis.com>
2025-09-09 17:01:33 -0400

    basic reading of saved entries

--- src/components/CheckEntry.vue
+++ src/components/CheckEntry.vue
@@ -2,6 +2,9 @@
 import type { DRAWING, PLAYER_ENTRIES } from '../types';
 import Result from './Result.vue';
 import { computed } from 'vue';
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
 
 const CURRENT_DRAWING:DRAWING = {
     name: "powerball",
@@ -10,40 +13,29 @@ const CURRENT_DRAWING:DRAWING = {
     extra: 17
 };
 
-const HOPES: PLAYER_ENTRIES = {
-    name: "powerball",
-    date: "2025-09-06",
-    attempts: [
-        {
-            base: [5, 19 ,27, 36, 68],
-            extra: 9
-        },
-        {
-            base: [2, 15 ,59, 61, 69],
-            extra: 18
-        },
-        {
-            base: [9, 34, 57, 63, 68],
-            extra: 18
-        },
-        {
-            base: [25, 41, 49, 56, 67],
-            extra: 9
-        },
-        {
-            base: [4, 13 , 32, 34, 54],
-            extra: 12
-        }
-    ]
-}
 const USDate = computed(() => {
     return new Intl.DateTimeFormat("en-US").format( new Date(CURRENT_DRAWING.date))
+})
+
+const DATE = computed<string>(() => {
+    return route.query.date?.toString() ?? ''
+})
+
+const t = computed<PLAYER_ENTRIES|null>(() => {
+    const LOCAL = localStorage.getItem(DATE.value)
+    if (LOCAL) {
+        return JSON.parse(LOCAL)
+    }
+    return null
 })
 </script>
 <template>
     <h1>{{ CURRENT_DRAWING.name }}</h1>
     <p>{{ USDate }}</p>
-    <template v-for="ENTRY in HOPES.attempts">
-        <Result :drawing="CURRENT_DRAWING" :attempt="ENTRY"/>
-    </template>
+    <div v-if="t">
+        <template v-for="ENTRY in t.attempts">
+            <Result :drawing="CURRENT_DRAWING" :attempt="ENTRY"/>
+        </template>    
+    </div>
+    
 </template>
