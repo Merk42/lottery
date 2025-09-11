1438ad183859600e4d0d6164f60a8925600a7723
Merk42 <mark_c@markecurtis.com>
2025-09-11 11:43:47 -0400

    basic layout updates

--- src/components/PickLottery.vue
+++ src/components/PickLottery.vue
@@ -6,6 +6,8 @@ interface linkgroup {
     checknext:string;
     enterprev: string;
     enternext: string;
+    prevdate: string;
+    nextdate: string;
 }
 const powerballlinks = computed<linkgroup>(() => {
     let prev = "";
@@ -33,7 +35,9 @@ const powerballlinks = computed<linkgroup>(() => {
         checkprev: `/etc/lottery/check/powerball/${prev}`,
         checknext: `/etc/lottery/check/powerball/${next}`,
         enterprev: `/etc/lottery/enter/powerball/${prev}`,
-        enternext: `/etc/lottery/enter/powerball/${next}`
+        enternext: `/etc/lottery/enter/powerball/${next}`,
+        prevdate: USDate(prev),
+        nextdate: USDate(next)
     }
 })
 
@@ -61,7 +65,9 @@ const megamillionslinks = computed<linkgroup>(() => {
         checkprev: `/etc/lottery/check/megamillions/${prev}`,
         checknext: `/etc/lottery/check/megamillions/${next}`,
         enterprev: `/etc/lottery/enter/megamillions/${prev}`,
-        enternext: `/etc/lottery/enter/megamillions/${next}`
+        enternext: `/etc/lottery/enter/megamillions/${next}`,
+        prevdate: USDate(prev),
+        nextdate: USDate(next)
     }
 })
 
@@ -80,36 +86,35 @@ function nextDrawingDate(target:number):string{
     }
     return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${(date.getDate()).toString().padStart(2, '0')}`
 }
+
+function USDate(date:string) {
+    const TIME = `${date}T00:00:00`;
+    return new Intl.DateTimeFormat("en-US").format( new Date(TIME))
+}
 </script>
 <template>
     <section>
-        <h2>Powerball</h2>
         <img src="/powerball.png"/>
-
-<div class="links">
-<RouterLink :to="{ path: powerballlinks.checkprev }" >Check</RouterLink>    
-<RouterLink :to="{ path: powerballlinks.enterprev }">Enter</RouterLink>
-</div>
-
-<div class="links">
-<RouterLink :to="{ path: powerballlinks.checknext }">Check</RouterLink>    
-<RouterLink :to="{ path: powerballlinks.enternext }">Enter</RouterLink>
-</div>
-</section>
-<section>
-    <h2>Megamillions</h2>
-<img src="/megamillions.png"/>
-
-<div class="links">
-<RouterLink :to="{ path: megamillionslinks.checkprev }">Check</RouterLink>    
-<RouterLink :to="{ path: megamillionslinks.enterprev }">Enter</RouterLink>
-</div>
-
-<div class="links">
-<RouterLink :to="{ path: megamillionslinks.checknext }">Check</RouterLink>    
-<RouterLink :to="{ path: megamillionslinks.enternext }">Enter</RouterLink>
-</div>
-</section>
+        <div class="links">
+            <h2>{{ powerballlinks.prevdate }}</h2>
+            <RouterLink :to="{ path: powerballlinks.checkprev }">Check</RouterLink>    
+            <RouterLink :to="{ path: powerballlinks.enterprev }">Enter</RouterLink>
+            <h2>{{ powerballlinks.nextdate }}</h2>
+            <RouterLink :to="{ path: powerballlinks.checknext }">Check</RouterLink>    
+            <RouterLink :to="{ path: powerballlinks.enternext }">Enter</RouterLink>
+        </div>
+    </section>
+    <section>
+        <img src="/megamillions.png"/>
+        <div class="links">
+            <h2>{{ megamillionslinks.prevdate }}</h2>
+            <RouterLink :to="{ path: megamillionslinks.checkprev }">Check</RouterLink>    
+            <RouterLink :to="{ path: megamillionslinks.enterprev }">Enter</RouterLink>
+            <h2>{{ megamillionslinks.nextdate }}</h2>
+            <RouterLink :to="{ path: megamillionslinks.checknext }">Check</RouterLink>    
+            <RouterLink :to="{ path: megamillionslinks.enternext }">Enter</RouterLink>
+        </div>
+    </section>
 </template>
 
 <style scoped>
@@ -119,14 +124,27 @@ function nextDrawingDate(target:number):string{
     gap:1rem;
     margin-inline: auto;
     max-width: 600px;
+    margin-top:1rem;
+}
+.links h2 {
+    grid-column: -1 / 1;
+    margin: 0;
 }
 a {
     display: inline-block;
-    border: 1px solid black;
+    border: .25rem solid canvasText;
+    padding: .5rem 1rem;
+    border-radius: 1rem;
+    text-decoration: none;
+    color: canvasText;
+    font-weight: bold;
 }
 img{
     display:block;
-    max-width: 100%;
+    max-width: 250px;
     margin-inline:auto;
+}
+section {
+    margin-bottom:2rem;
 }
 </style>
